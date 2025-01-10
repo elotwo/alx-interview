25dc1287de2d19bdd7a171bd9496702c67d85148
elotwo <obiagwu94@gmail.com>
2025-01-11 00:22:17 +0100

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,5 +2,22 @@
 """
 Island
 """
-def island_perimeter(grid):
 
+
+def island_perimeter(grid):
+    """
+    Function that returns the perimeter of the island described in grid
+    """
+    perimiter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimiter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimiter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimiter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimiter += 1
+    return perimiter
