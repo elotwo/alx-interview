326b61cebccc234d0c1299473d2b5d9d4c355429
elotwo <obiagwu94@gmail.com>
2025-01-10 23:48:30 +0100

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+"""
+making change
+"""
+
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
     coins.sort(reverse=True)
-    change = 0 
+    change = 0
     for coin in coins:
         if total <= 0:
             break
         change += total // coin
         total = total % coin
-    if total != 0 :
+    if total != 0:
         return -1
-    return change               
+    return change
